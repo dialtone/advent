ac615cde575e3397b229af856aae4eb93222330e
dialtone <dialtone@adroll.com>
2019-12-29 09:56:55 +0100

    all programs benched now, pretty good times

--- advent_bench/benches/bench.rs
+++ advent_bench/benches/bench.rs
@@ -7,7 +7,23 @@ use criterion::{criterion_group, criterion_main, Criterion};
 fn criterion_benchmark(c: &mut Criterion) {
     let input =
         futures::executor::block_on(async_std::fs::read_to_string("sum-of-primes")).unwrap();
-    c.bench_function("p1_me", |b| b.iter(|| run(&input, 100_000)));
+    c.bench_function("sum-of-primes", |b| b.iter(|| run(&input, vec![100_000])));
+    let input = futures::executor::block_on(async_std::fs::read_to_string("factor")).unwrap();
+    c.bench_function("factor-small", |b| {
+        b.iter(|| run(&input, vec![2_147_483_647]))
+    });
+    c.bench_function("factor-big", |b| {
+        b.iter(|| run(&input, vec![19_201_644_899]))
+    });
+
+    let input = futures::executor::block_on(async_std::fs::read_to_string("isqrt")).unwrap();
+    c.bench_function("isqrt", |b| b.iter(|| run(&input, vec![130])));
+
+    let input = futures::executor::block_on(async_std::fs::read_to_string("ackermann")).unwrap();
+    c.bench_function("ackermann", |b| b.iter(|| run(&input, vec![3, 6])));
+
+    let input = futures::executor::block_on(async_std::fs::read_to_string("divmod")).unwrap();
+    c.bench_function("divmod", |b| b.iter(|| run(&input, vec![1024, 3])));
 }
 
 criterion_group!(
--- advent_bench/src/solution.rs
+++ advent_bench/src/solution.rs
@@ -1,8 +1,10 @@
 use crate::intcode;
 
-pub fn run(input: &str, arg: isize) -> isize {
+pub fn run(input: &str, args: Vec<isize>) -> isize {
     let mut computer = intcode::Computer::from(&input);
-    computer.write_input(arg);
+    for arg in args {
+        computer.write_input(arg);
+    }
 
     loop {
         if computer.has_output() {
@@ -17,6 +19,6 @@ async fn test() -> async_std::io::Result<()> {
     use super::*;
 
     let input = async_std::fs::read_to_string("sum-of-primes").await?;
-    assert_eq!(run(&input, 100_000), 454_396_537);
+    assert_eq!(run(&input, vec![100_000]), 454_396_537);
     Ok(())
 }
